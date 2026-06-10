c7f9799de2374c8417800c1f9b88273e41d05f9a
Brock <45976315+brockcataldi@users.noreply.github.com>
2026-06-10 00:02:55 -0400

    Removal of boilerplate, and fixing some configs

---
+++ src/lib/ui/button/Button.stories.svelte
@@ -0,0 +1,26 @@
+<script module lang="ts">
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	// import { fn } from 'storybook/test';
+	// import type { ComponentProps } from 'svelte';
+
+	import Button from './Button.svelte';
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: 'UI/Button',
+		component: Button,
+		tags: ['autodocs']
+		// argTypes: {
+		// 	backgroundColor: { control: 'color' },
+		// 	size: {
+		// 		control: { type: 'select' },
+		// 		options: ['small', 'medium', 'large']
+		// 	}
+		// },
+		// args: {
+		// 	onclick: fn()
+		// }
+	});
+</script>
+
+<Story name="Default">Hello World!</Story>
---
+++ src/lib/ui/button/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type ButtonProps = {
+		children: Snippet;
+	};
+
+	let { children }: ButtonProps = $props();
+</script>
+
+<button class="text-red-700">
+	{@render children()}
+</button>
